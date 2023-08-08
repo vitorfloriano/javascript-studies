8594ea5ccb3be254d9509015e67aa27ab0058f9a
vitorfloriano <vitorflorianoc@gmail.com>
2023-08-07 22:27:17 -0300

    Add comments to script Just to help me understand what's going on.

--- js-info-logical-operators-ex/script.js
+++ js-info-logical-operators-ex/script.js
@@ -1,17 +1,25 @@
-
+// ask user for name
 let who = prompt("Who'is there?");
 
+// if name is "Admin"
 if (who === "Admin") {
+    // ask user for password
     let login = prompt("password");
-if (login === "TheMaster") {
-    alert("Welcome");
-} else if (login === null) {
-    alert('Canceled')
-} else {
-    alert ('wrong password')
-}
+
+    // if password is "TheMaster"
+    if (login === "TheMaster") {
+        alert("Welcome");
+    // if password is empty
+    } else if (login === null) {
+        alert('Canceled')
+    // if password is wrong
+    } else {
+        alert ('wrong password')
+    }
+// if name is empty
 } else if (who === null) {
     alert('Canceled');
+// if name is wrong
 } else {
     alert('I dont know you');
 }
