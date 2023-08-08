a9bdd21b9e2dae991866d40cdc30dfcdc97545e4
vitorfloriano <vitorflorianoc@gmail.com>
2023-08-07 21:51:08 -0300

    Format code according to my taste Just nesting some code blocks a la python.

--- js-info-logical-operators-ex/script.js
+++ js-info-logical-operators-ex/script.js
@@ -2,17 +2,16 @@
 let who = prompt("Who'is there?");
 
 if (who === "Admin") {
-let login = prompt("password");
+    let login = prompt("password");
 if (login === "TheMaster") {
-alert("Welcome");
+    alert("Welcome");
 } else if (login === null) {
-alert('Canceled')
+    alert('Canceled')
 } else {
-alert ('wrong password')
+    alert ('wrong password')
 }
-
 } else if (who === null) {
-alert('Canceled');
+    alert('Canceled');
 } else {
-alert('I dont know you');
+    alert('I dont know you');
 }
